37344dbb5207aeb2895dee16e812e3bafbaf49e0
Adelino Lourenço <adelino.lourenco@xvia.com.br>
2026-07-08 11:36:00 -0300

    add plugin 'nvim-treesitter'

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,42 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  build = ":TSUpdate",
+
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+
+        "javascript",
+        "typescript",
+        "tsx",
+
+        "json",
+        "yaml",
+        "toml",
+
+        "html",
+        "css",
+
+        "markdown",
+        "markdown_inline",
+
+        "bash",
+        "dockerfile",
+
+        "gitignore",
+      },
+
+      highlight = {
+        enable = true,
+      },
+
+      indent = {
+        enable = true,
+      },
+    })
+  end,
+}
