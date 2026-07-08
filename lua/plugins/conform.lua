7c1afa670ebe95f7c92ef84dda5e444163c2d673
Adelino Lourenço <adelino.lourenco@xvia.com.br>
2026-07-08 13:29:14 -0300

    add plugin 'conform.nvim'

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,25 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
+
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
+
+      json = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+    },
+
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  },
+}
