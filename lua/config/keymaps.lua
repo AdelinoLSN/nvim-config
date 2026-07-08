719e9581c6c6ddc21efafe272c46472bee8acd15
Adelino Lourenço <adelino.lourenco@xvia.com.br>
2026-07-08 10:53:44 -0300

    add plugin 'telescope.nvim'

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,20 @@ vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Close" })
 
 -- Neotree
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "Explorer" })
+
+-- Telescope
+vim.keymap.set("n", "<leader>ff", function()
+  require("telescope.builtin").find_files()
+end, { desc = "Find files" })
+
+vim.keymap.set("n", "<leader>fg", function()
+  require("telescope.builtin").live_grep()
+end, { desc = "Find text" })
+
+vim.keymap.set("n", "<leader>fb", function()
+  require("telescope.builtin").buffers()
+end, { desc = "Buffers" })
+
+vim.keymap.set("n", "<leader>fh", function()
+  require("telescope.builtin").help_tags()
+end, { desc = "Help" })
