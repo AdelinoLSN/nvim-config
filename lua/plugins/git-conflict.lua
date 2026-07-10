10127a092f9ae8d07389e51406d6cdc857882200
Adelino Lourenço <adelino.lourenco@xvia.com.br>
2026-07-10 10:43:16 -0300

    add plugin 'git-conflict.nvim'

---
+++ lua/plugins/git-conflict.lua
@@ -0,0 +1,15 @@
+return {
+	"akinsho/git-conflict.nvim",
+	version = "*",
+
+	opts = {
+		default_mappings = true,
+		default_commands = true,
+		disable_diagnostics = true,
+		list_opener = "copen",
+		highlights = {
+			incoming = "DiffAdd",
+			current = "DiffText",
+		},
+	},
+}
